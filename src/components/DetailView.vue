1c8aaf1df070e1e2df20a5cbb5a4dfdaafdcc581
Mateo <mateoej12@gmail.com>
2022-04-04 14:54:33 -0700

    Added Bootstrap cdn to public.

---
+++ src/components/DetailView.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Detail View</h1>
+</template>
+
+<script>
+export default {
+  name: "DetailView",
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
     <Main />
+    <div class="row">
+      <DetailView />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Main from "@/components/Main.vue";
+import DetailView from "@/components/DetailView.vue";
 import { mapState } from "vuex";
 
 export default {
   name: "Home",
   components: {
     Main,
+    DetailView,
   },
   mounted() {
     this.$store.dispatch("loadData");
