ef4a0ffc042fc44930239bd37d9ed7d6f9835429
Mateo <mateoej12@gmail.com>
2022-04-04 15:47:31 -0700

    Added the list view where all the weather card components are housed.

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>Detail View</h1>
+  <div class="p-3 m-3">
+    <h3>Mars Sol {{ this.sol }}</h3>
+    <p>Earth Date: {{ this.date }}</p>
+
+    <p>Max Temperature (F): {{ this.maxTemp }}</p>
+    <p>Min Temperature (F): {{ this.minTemp }}</p>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   name: "DetailView",
+  computed: {
+    ...mapState(["weatherData"]),
+  },
+  props: {
+    sol: Number,
+    date: String,
+    maxTemp: String,
+    minTemp: String,
+  },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="home">
-    <Main />
-    <div class="row">
-      <DetailView />
+    <h1>Mars Weather App</h1>
+    <div class="row gap-3 mx-auto">
+      <div
+        v-for="sol in weatherData"
+        :key="sol.Sol"
+        class="col border border-danger border-3 solid rounded-3"
+      >
+        <DetailView
+          :sol="sol.Sol"
+          :maxTemp="sol.max"
+          :minTemp="sol.min"
+          :date="sol.Date"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Main from "@/components/Main.vue";
-import DetailView from "@/components/DetailView.vue";
 import { mapState } from "vuex";
+// @ is an alias to /src
+
+import DetailView from "@/components/DetailView.vue";
+// import { mapState } from "vuex";
 
 export default {
   name: "Home",
   components: {
-    Main,
     DetailView,
   },
-  mounted() {
-    this.$store.dispatch("loadData");
-  },
+
+  // mounted() {
+  //   this.$store.dispatch("loadData");
+  // },
   computed: {
     ...mapState(["weatherData"]),
   },
